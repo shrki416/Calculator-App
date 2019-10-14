2dac37126c7bd02d7d8c81d5671ada2a11ac3f63
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-10-14 12:49:05 -0400

    functionality - work in progess

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-// alert('connected!');
+const calculator = document.querySelector('.calculator');
+const currentDisplay = document.querySelector(".current-display");
+const previousDisplay = document.querySelector('.previous-display');
+
+calculator.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const calcButton = e.target
+        const action = calcButton.dataset.action
+        const calcBtnValue = calcButton.textContent
+        const displayedKey = currentDisplay.textContent
+        
+        if (!action) {
+            currentDisplay.textContent = displayedKey + calcBtnValue
+        }
+
+        if (
+          action === "add" ||
+          action === "subtract" ||
+          action === "divide" ||
+          action === "multiply"
+        ) {
+          console.log("operator key!");
+        }
+
+        if (action === "decimal") {
+            console.log('decimal key!');
+            if (currentDisplay === '.') return
+            currentDisplay.textContent = displayedKey + '.'
+        }
+
+        if (action === "clear") {
+          console.log("clear key!");
+          currentDisplay.textContent = ''
+          previousDisplay.textContent = ''
+        }
+
+        if (action === "calculate") {
+          console.log("equal key!");
+        }
+
+        if (action === "delete") {
+          console.log("delete key!");
+        }
+    }
+})
+
+
+
+
