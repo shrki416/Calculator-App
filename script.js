69b2e933226ac141a20aea288f80267699bf9ca2
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-10-17 10:56:38 -0400

    refactor script.js

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
-const calculator = document.querySelector('.calculator')
-const operationButtons = document.querySelectorAll('[data-operation]')
+document.body.onload = function(){
+
+
 const calculatorNumberButtons = document.querySelectorAll('[data-number]')
-const currentDisplay = document.querySelector('.current-display')
-const previousDisplay = document.querySelector('.previous-display')
+const calculatorOperationButtons = document.querySelectorAll('[data-operation]')
 
-let firstOperand = null;
-let secondOperand = null;
+compute();
+removeLastChar();
+updateDisplay();
+chooseOperator();
+appendNumber();
 
-operationButtons.forEach(button => {
-  button.addEventListener('click', e => {
-    const operation = button.dataset.action
-    switch (operation){
-      case 'add':
-        // addition logic here
-        console.log("add key was pressed");
-        break;
-      case 'subtract':
-        // subtract logic here
-        console.log('subtract key was pressed');
-        break;
-      case 'divide':
-        // divide logic here
-        console.log('divide key was pressed');
-        break;
-      case 'multiply':
-        // multiplication logic here
-        console.log('multiply key was pressed');
-        break;
-      case 'delete':
-        // delete logic here
-        console.log('delete key was pressed');
-        break;
-      default:
-        console.log('wrong operation');
-    }
+
+function appendNumber(){
+  calculatorNumberButtons.forEach(button => {
+    button.addEventListener("click", e => {
+      currentDisplay = document.querySelector("[data-current-display]");
+      if (
+        button.textContent === "." &&
+        currentDisplay.textContent.includes(".")
+      )
+        return;
+      currentDisplay.textContent =
+        currentDisplay.textContent + button.textContent;
+    });
+  });
+}
+
+function chooseOperator(){
+  calculatorOperationButtons.forEach(button => {
+    button.addEventListener("click", e => {
+      const operator = button.dataset.action
+      switch (operator) {
+        case "add":
+          console.log("add");
+          break;
+        case "subtract":
+          console.log("subtract");
+          break;
+        case "divide":
+          console.log("divide");
+          break;
+        case "multiply":
+          console.log("multiply");
+          break;
+        default:
+          console.log('no operation')
+      }
+    });
+  });
+}
+
+function updateDisplay(){
+  const clearDisplay = document.querySelector('[data-all-clear]')
+  clearDisplay.addEventListener('click', e => {
+    currentDisplay.textContent = '';
   })
-})
+}
 
-calculatorNumberButtons.forEach(button => {
-  button.addEventListener('click', e => {
-    const numberButtons = button.textContent
-    const display = currentDisplay.textContent
-    if (numberButtons === '.' && display.includes('.')) return
-    currentDisplay.textContent = display + numberButtons
-
-    console.log('number key was pressed')
+function removeLastChar(button){
+  const deleteButton = document.querySelector('[data-delete]')
+  deleteButton.addEventListener('click', e => {
   })
-})
+}
 
-const clearButton = document.querySelector('[data-all-clear]')
-clearButton.addEventListener('click', e => {
-  currentDisplay.textContent = ''
-  previousDisplay.textContent = ''
-  console.log('clear button was pressed')
-})
+function compute(){
+  const equalButton = document.querySelector('[data-equals]')
+  equalButton.addEventListener('click', e => {
+    console.log('equal button is pressed')
+  })
+}
 
-const equalButton = document.querySelector('[data-equals]')
-equalButton.addEventListener('click', e => {
-  console.log('equal button was pressed')
-})
+}
