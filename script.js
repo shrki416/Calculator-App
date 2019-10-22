1ca1884ad14f63c33eaf711d1aa2176f2b05ea3a
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-10-22 11:24:19 -0400

    calc functionality cont..

--- script.js
+++ script.js
@@ -1,92 +1,89 @@
-document.body.onload = function(){
+document.body.onload = function() {
+  const calculatorNumberButtons = document.querySelectorAll("[data-number]");
+  const calculatorOperationButtons = document.querySelectorAll(
+    "[data-operation]"
+  );
+  previousDisplay = document.querySelector("[data-previous-display]");
+  currentDisplay = document.querySelector("[data-current-display]");
 
-const calculatorNumberButtons = document.querySelectorAll('[data-number]')
-const calculatorOperationButtons = document.querySelectorAll('[data-operation]')
-previousDisplay = document.querySelector("[data-previous-display]");
-currentDisplay = document.querySelector("[data-current-display]");
+  appendNumber();
+  compute();
+  removeLastChar();
+  allClear();
 
-appendNumber();
-compute();
-// result();
-removeLastChar();
-updateDisplay();
-
-function appendNumber() {
-  calculatorNumberButtons.forEach(button => {
-    button.addEventListener("click", e => {
-      if (
-        button.textContent === "." &&
-        currentDisplay.textContent.includes(".")
-      )
-        return;
-      currentDisplay.textContent =
-        currentDisplay.textContent + button.textContent;
-    });
-  });
-}
-
-function compute() {
-  
-  calculatorOperationButtons.forEach(button => {
-    button.addEventListener("click", e => {
-      const operator = button.dataset.action
-      
-      previousDisplay.textContent = currentDisplay.textContent + button.textContent;
-      let firstOperand = parseFloat(previousDisplay.textContent);
-
-      console.log(firstOperand, currentDisplay.textContent)
-
-      if (currentDisplay.textContent != null) {
-        previousDisplay.textContent = firstOperand + button.textContent;
-        currentDisplay.textContent = ''
-      } 
-
-      switch (operator) {
-        case "+":
-          // firstOperand + secondOperand
-          break;
-        case "-":
-          // firstOperand - secondOperand
-          break;
-        case "/":
-          // firstOperand / secondOperand
-          break;
-        case "*":
-          // firstOperand * secondOperand
-          break;
-        default:
+  function appendNumber() {
+    calculatorNumberButtons.forEach(button => {
+      button.addEventListener("click", e => {
+        if (
+          button.textContent === "." &&
+          currentDisplay.textContent.includes(".")
+        )
           return;
-      }
-      const equalButton = document.querySelector("[data-equals]");
-      equalButton.addEventListener("click", e => {
-        console.log("equal button is pressed");
-        console.log(firstOperand, currentDisplay.textContent)
+        currentDisplay.textContent =
+          currentDisplay.textContent + button.textContent;
       });
     });
-  });
-}
+  }
 
-// function result() {
-//   const equalButton = document.querySelector("[data-equals]");
-//   equalButton.addEventListener("click", e => {
-//     console.log("equal button is pressed");
-//   });
-// }
+  function compute() {
+    calculatorOperationButtons.forEach(button => {
+      button.addEventListener("click", e => {
+        const operator = button.dataset.action;
+        let result;
 
-function updateDisplay() {
-  const clearDisplay = document.querySelector('[data-all-clear]')
-  clearDisplay.addEventListener('click', e => {
-    currentDisplay.textContent = '';
-    previousDisplay.textContent = '';
-  })
-}
+        previousDisplay.textContent =
+          currentDisplay.textContent + button.textContent;
+        let firstOperand = parseFloat(previousDisplay.textContent);
 
-function removeLastChar() {
-  const deleteButton = document.querySelector('[data-delete]')
-  deleteButton.addEventListener('click', e => {
-    let displayString = currentDisplay.textContent
-    let deleteLastCharacter = displayString.slice(0, -1)
-    currentDisplay.textContent = deleteLastCharacter;
-  })
-}
-}
+        console.log(firstOperand, currentDisplay.textContent);
+
+        if (currentDisplay.textContent != null) {
+          previousDisplay.textContent = firstOperand + button.textContent;
+          currentDisplay.textContent = "";
+        }
+
+        let secondOperand = parseFloat(currentDisplay.textContent);
+
+        switch (operator) {
+          case "+":
+            result = firstOperand + secondOperand;
+            break;
+          case "-":
+            result = firstOperand - secondOperand;
+            break;
+          case "/":
+            result = firstOperand / secondOperand;
+            break;
+          case "*":
+            result = firstOperand * secondOperand;
+            break;
+          default:
+            return;
+        }
+        const equalButton = document.querySelector("[data-equals]");
+        equalButton.addEventListener("click", e => {
+          currentDisplay.textContent = result;
+          console.log("equal button is pressed");
+          console.log(firstOperand, currentDisplay.textContent);
+        });
+      });
+    });
+  }
+
+  function allClear() {
+    const clearDisplay = document.querySelector("[data-all-clear]");
+    clearDisplay.addEventListener("click", e => {
+      currentDisplay.textContent = "";
+      previousDisplay.textContent = "";
+    });
+  }
+
+  function removeLastChar() {
+    const deleteButton = document.querySelector("[data-delete]");
+    deleteButton.addEventListener("click", e => {
+      let displayString = currentDisplay.textContent;
+      let deleteLastCharacter = displayString.slice(0, -1);
+      currentDisplay.textContent = deleteLastCharacter;
+    });
+  }
+};
