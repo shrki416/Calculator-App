17fcf4b96ff23d340ddbf1978756279a679cb31d
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2019-10-25 13:20:19 -0400

    refactored code

--- script.js
+++ script.js
@@ -5,25 +5,21 @@ document.body.onload = function() {
 
   isOperatorClicked = false;
 
-  const calculatorNumberButtons = document.querySelectorAll("[data-number]");
-  const calculatorOperatorButtons = document.querySelectorAll(
-    "[data-operation]"
-  );
-  const equalButton = document.querySelector("[data-equal]");
-  const allClearButton = document.querySelector("[data-all-clear]");
-  const deleteLastCharacter = document.querySelector("[data-delete]");
-  previousDisplay = document.querySelector("[data-previous-display]");
-  currentDisplay = document.querySelector("[data-current-display]");
+  const calculatorNumberButtons   = document.querySelectorAll("[data-number]");
+  const calculatorOperatorButtons = document.querySelectorAll("[data-operation]");
+  const equalButton               = document.querySelector("[data-equal]");
+  const allClearButton            = document.querySelector("[data-all-clear]");
+  const deleteLastCharacter       = document.querySelector("[data-delete]");
+  previousDisplay                 = document.querySelector("[data-previous-display]");
+  currentDisplay                  = document.querySelector("[data-current-display]");
 
   calculatorNumberButtons.forEach(button => {
     button.addEventListener("click", e => {
       if (
         button.textContent === "." &&
         currentDisplay.textContent.includes(".")
-      )
-        return;
-      currentDisplay.textContent =
-        currentDisplay.textContent + button.textContent;
+      ) return;
+      currentDisplay.textContent = currentDisplay.textContent + button.textContent;
     });
   });
 
@@ -31,7 +27,7 @@ document.body.onload = function() {
     button.addEventListener("click", e => {
       operator = button.textContent;
       console.log("operator: ", operator);
-      if ((isOperatorClicked = true)) {
+      if (isOperatorClicked = true) {
         firstOperand = parseFloat(currentDisplay.textContent);
         previousDisplay.textContent = firstOperand + operator;
         currentDisplay.textContent = "";
@@ -42,10 +38,7 @@ document.body.onload = function() {
   });
 
   deleteLastCharacter.addEventListener("click", e => {
-    currentDisplay.textContent = currentDisplay.textContent.substring(
-      0,
-      currentDisplay.textContent.length - 1
-    );
+    currentDisplay.textContent = currentDisplay.textContent.substring(0, currentDisplay.textContent.length - 1);
   });
 
   allClearButton.addEventListener("click", e => {
